f5dabf6654ef7b62b2616d687a6a99a1d576ec5c
iamharshgoel <harshgoel0012@gmail.com>
2020-04-18 21:30:55 +0530

    use of shebang file and sys module

---
+++ hello_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+def Hello(name):
+    name = name + '!!!!!!'
+    print('Hello', name)
+
+
+def main():
+    Hello(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
